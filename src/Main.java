1f5b85dbf266285269d84e0d93a7825641c76a9b
Paulina <paulina.winkowska@gmail.com>
2014-03-29 09:59:00 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class Main {
     public static void main (String[] args) {
-        //Tasks.lab03(new FigureMaker());
+        Tasks.lab03(new FigureMaker());
         Tasks.lab04(new FigureMaker());
     }
 }
--- src/Tasks.java
+++ src/Tasks.java
@@ -6,6 +6,13 @@ import java.util.*;
 
 public class Tasks {
     public static void lab03 (FigureMaker maker) {
+        listWithForEachAndIterator(maker);
+        treatGreenAndRedAsTheSame(maker);
+        listInOrderOfDecreasingArea(maker);
+        listInOrderOfAddingElements(maker);
+    }
+
+    private static void listWithForEachAndIterator(FigureMaker maker) {
         maker.makeArrayList();
         System.out.println("Listed with for each:");
         CollectionControl.listWithForEach(maker.getArrayList());
@@ -13,34 +20,57 @@ public class Tasks {
         System.out.println("\n \n \n In order to area, listed with iterator:");
         Collections.sort(maker.getArrayList(), new FigureAreaComparator());
         CollectionControl.listWithIterator(maker.getArrayList());
+    }
 
+    private static void treatGreenAndRedAsTheSame(FigureMaker maker) {
         System.out.println("\n \n \n Green and red figures treated as the same:");
         maker.makeHashSet();
         CollectionControl.listWithForEach(maker.getSet());
+    }
 
+    private static void listInOrderOfDecreasingArea(FigureMaker maker) {
         System.out.println("\n \n \n In order of decreasing area:");
         maker.makeTreeSet();
         CollectionControl.listWithForEach(maker.getSet());
+    }
 
+    private static void listInOrderOfAddingElements(FigureMaker maker) {
         System.out.println("\n \n \n In order of adding elements");
         maker.makeLinkedTreeSet();
         CollectionControl.listWithForEach(maker.getSet());
     }
 
+
+
+
+
+
     public static void lab04 (FigureMaker maker) {
+        mapByFunction(maker);
+        mapWithFunctionAsKey(maker);
+        filterCollection(maker);
+    }
+
+    private static void mapByFunction(FigureMaker maker) {
         System.out.println("\n \n \n Mapping by function that doubles areas:");
         maker.makeArrayList();
         System.out.println(RichClass.doubleArea(maker.getArrayList()));
+    }
 
+    private static void mapWithFunctionAsKey(FigureMaker maker) {
         System.out.println("\n \n \n Mapping with function as key (figures of the same type have the same key):");
         maker.makeArrayList();
         System.out.println(RichClass.groupByFigure(maker.getArrayList()));
+    }
 
+    private static void filterCollection(FigureMaker maker) {
         System.out.println("\n \n \n Filtering collection of figures to ArrayList of Circles:");
         maker.makeArrayList();
         Collection<Circle> arrayOfCircles = RichClass.select(maker.getArrayList(), Circle.class);
         System.out.println(arrayOfCircles);
     }
+
+
 
 
     private static class FigureAreaComparator implements Comparator<Figure> {
